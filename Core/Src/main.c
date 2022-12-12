998c3bd66af09df5932a34f8da03a935c3af0f67
AdamPaech <adam.paech@gmail.com>
2022-12-12 14:11:44 +0100

    fnSingleMotionAction definition

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -57,6 +57,26 @@ uint8_t sUserMessage[4];
 const uint8_t sErrorMessage[] = "UART ERROR\r\n";
 const uint8_t sMoveCommand[] = "MOVE";
 const uint8_t sStopCommand[] = "STOP";
+char chTab[4];
+char phrase[10];
+uint8_t length = 0;
+
+//MODE SELECTION
+const uint8_t sSingle_mode[] = "SNGL";
+const uint8_t sSerial_mode[] = "SERL";
+
+//LEG SELECTION
+const uint8_t sRight[] = "RGHT";
+const uint8_t sLeft[] = "LEFT";
+
+//MINMAX VALUES
+const uint32_t iMax_angle = 90;
+const uint32_t iMin_angle = 0;
+
+//STATE VALUES
+volatile uint8_t iFlag = 0;
+volatile uint8_t iMode = 100; // default:100, 0 - SINGLE MOTION MODE, 1 - SERIAL MOTION MODE
+volatile uint8_t iSelected_leg = 100; // default:100, 0 - LEFT LEG, 1 - RIGHT LEG
 
 //CAN VARIABLES
 CAN_TxHeaderTypeDef TxHeader;
@@ -71,6 +91,7 @@ CAN_FilterTypeDef CANFilter;
 //STATE MACHINE VARIABLES
 uint8_t iMachineStatus = 100;
 uint8_t iHomingStatus = 100;
+uint8_t iSingleMachineStatus = 100;
 
 //ENCODER VARIABLES
 uint16_t iEncCountReal=0;
@@ -330,6 +351,7 @@ void fnInit(){
 	}
 }
 
+
 //CALCULATING ENCODER'S COUNTS TO ANGLE
 float fnEncCounts2Angle(uint16_t iCounts)
 {
@@ -390,6 +412,61 @@ void fnMoveAbsolute(uint32_t iNumber){
 
 }
 
+void fnSingleMotionAction(){
+	switch (iSingleMachineStatus){
+	case 10:
+		//START SUPPLY
+		TxHeader.StdId = 0x60A;
+		TxHeader.DLC = 8;
+		TxData[0] = 0x22;
+		TxData[1] = 0x40;
+		TxData[2] = 0x60;
+		TxData[3] = 0x00;
+		TxData[4] = 0x1F;
+		TxData[5] = 0x00;
+		TxData[6] = 0x00;
+		TxData[7] = 0x00;
+
+		if(HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK){
+			fnLEDsErrorState();
+			Error_Handler();
+		}
+		else{
+			iSingleMachineStatus = 20;
+		}
+		break;
+
+	case 20:
+		//MOVE
+		fnMoveAbsolute(iPosition);
+		iSingleMachineStatus = 30;
+		break;
+
+	case 30:
+		//STOP SUPPLY
+		TxHeader.StdId = 0x60A;
+		TxHeader.DLC = 8;
+		TxData[0] = 0x22;
+		TxData[1] = 0x40;
+		TxData[2] = 0x60;
+		TxData[3] = 0x00;
+		TxData[4] = 0x0F;
+		TxData[5] = 0x00;
+		TxData[6] = 0x00;
+		TxData[7] = 0x00;
+
+		if(HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK){
+			fnLEDsErrorState();
+			Error_Handler();
+		}
+		else{
+			iSingleMachineStatus = 100;
+			HAL_TIM_Base_Stop_IT(&htim7);
+		}
+		break;
+	}
+}
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -409,6 +486,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 			HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
 			//HAL_TIM_Base_Stop_IT(&htim6);
 		}
+	}
+	else if(htim -> Instance == TIM7){
+		fnSingleMotionAction();
 	}
 
 }
@@ -440,39 +520,40 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART3_UART_Init();
-  MX_USB_OTG_FS_PCD_Init();
-  MX_CAN1_Init();
-  MX_TIM6_Init();
-  MX_TIM3_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART3_UART_Init();
+	MX_USB_OTG_FS_PCD_Init();
+	MX_CAN1_Init();
+	MX_TIM6_Init();
+	MX_TIM3_Init();
+	MX_TIM7_Init();
+	/* USER CODE BEGIN 2 */
 
 	// UART START
 	HAL_UART_Receive_IT(&huart3, sUserMessage, 4);
@@ -505,79 +586,79 @@ int main(void)
 	HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
 
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
 
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
-  /** Configure LSE Drive Capability
-  */
-  HAL_PWR_EnableBkUpAccess();
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 96;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Configure LSE Drive Capability
+	 */
+	HAL_PWR_EnableBkUpAccess();
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 4;
+	RCC_OscInitStruct.PLL.PLLN = 96;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	RCC_OscInitStruct.PLL.PLLR = 2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Activate the Over-Drive mode
+	 */
+	if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3|RCC_PERIPHCLK_CLK48;
-  PeriphClkInitStruct.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
-  PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48SOURCE_PLL;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3|RCC_PERIPHCLK_CLK48;
+	PeriphClkInitStruct.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+	PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48SOURCE_PLL;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -589,6 +670,27 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 			iMachineStatus = 0;
 			iHomingStatus = 0;
 			HAL_TIM_Base_Start_IT(&htim6);
+		}
+		//MODE SELECTION
+		else if(strncmp(sUserMessage, sSingle_mode, 4) == 0){
+			iMode = 0;
+			HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+		}
+
+		else if(strncmp(sUserMessage, sSerial_mode, 4) == 0){
+			iMode = 1;
+			HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		}
+
+		//LEG SELECTION
+		else if(strncmp(sUserMessage, sLeft, 4) == 0){
+			iSelected_leg = 0;
+			HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+		}
+
+		else if(strncmp(sUserMessage, sRight, 4) == 0){
+			iSelected_leg = 1;
+			HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 		}
 		else if (strncmp(sUserMessage, sMoveCommand,4) == 0) {
 			TxHeader.StdId = 0x60A;
@@ -624,9 +726,16 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 				Error_Handler();
 			}
 		}
-		else{
-			iPosition = (uint32_t)(atoi(sUserMessage));
-			fnMoveAbsolute(iPosition);
+		else if(sUserMessage[3] == 'P'){
+
+			if(iMode == 0){
+				iPosition = (uint32_t)(atoi(sUserMessage));
+				iSingleMachineStatus = 10;
+				HAL_TIM_Base_Start_IT(&htim7);
+				//fnSingleMotionAction();
+				//fnMoveAbsolute(iPosition);
+
+			}
 		}
 	}
 	else{
@@ -776,34 +885,34 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
  ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
